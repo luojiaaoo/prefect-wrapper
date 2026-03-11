472791c7108bff5679ed6498668dfd338861eb73
NOKIAO <675925864@qq.com>
2026-03-11 10:18:55 +0800

    简化参数

--- server/__main__.py
+++ server/__main__.py
@@ -9,10 +9,9 @@ def main():
     parser.add_argument("--port", type=int, default=4200, help="Server端口")
     mode_group = parser.add_mutually_exclusive_group()
     mode_group.add_argument("--docker", action="store_true", help="使用 Docker 启动")
-    mode_group.add_argument("--local", action="store_true", help="使用本地模式启动")
 
     args = parser.parse_args()
-    use_docker = True if args.docker else False if args.local else None
+    use_docker = True if args.docker else False
     run_server(host=args.host, port=args.port, use_docker=use_docker)
 
 
